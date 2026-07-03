887e92d314f21f8392697862ad9af3275bc5d989
Priyanka Priyadarshani <137867082+priyanka19999@users.noreply.github.com>
2026-07-04 00:14:56 +0530

    Get Last Element of the List
    Get Last Element of the List

---
+++ Basic-12/Solution_1.py
@@ -0,0 +1,9 @@
+#Python Program to Get the Last Element of the List
+
+
+Char = ["Ironman", "Thor", "Hulk", "Vision"]
+print(Char[-1]) # -ve indexing
+print(Char[-4]) # -ve indexing
+
+# Vision
+# Ironman
